25a69c67cd11be2ca3642bd2d0ab42310ccc77d8
Ignacio Avas <gmail@ignacioavas.com>
2014-05-17 16:38:34 -0300

    Skybox support for Wireframe rendering

--- FlappyBird/FlappyBird/Skybox.cpp
+++ FlappyBird/FlappyBird/Skybox.cpp
@@ -50,69 +50,71 @@ void Skybox::render(RenderMode mode)
 {
 	// Enable/Disable features
 	glPushAttrib(GL_ENABLE_BIT);
-	glToggle(GL_TEXTURE_2D, mode == TEXTURED_RENDER);
-	if(mode == WIREFRAME_RENDER)
 	{
-		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		glToggle(GL_TEXTURE_2D, mode == TEXTURED_RENDER);
+		if(mode == WIREFRAME_RENDER)
+		{
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		}
+		glDisable(GL_DEPTH_TEST);
+		glDisable(GL_LIGHTING);
+		glDisable(GL_BLEND);
+
+		// Just in case we set all vertices to white.
+		if(mode == TEXTURED_RENDER)
+		{
+			glColor4f(1,1,1,1);
+		} else {
+			glColor(Vector3::fromRGB(119, 196, 242));
+		}
+
+		const float deltaX = 0.25f;
+
+		// Render the front quad
+		glBindTexture(texture);
+		glBegin(GL_QUADS);
+		{
+			if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX*3, 0); } glVertex3f(  0.5f, -0.5f, -0.5f );
+			if(mode == TEXTURED_RENDER){ glTexCoord2f(1,		  0); } glVertex3f( -0.5f, -0.5f, -0.5f );
+			if(mode == TEXTURED_RENDER){ glTexCoord2f(1,		  1); } glVertex3f( -0.5f,  0.5f, -0.5f );
+			if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX*3, 1); } glVertex3f(  0.5f,  0.5f, -0.5f );
+		}
+		glEnd();
+
+		// Render the left quad
+		glBegin(GL_QUADS);
+		{
+			if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX*2, 0); } glVertex3f(  0.5f, -0.5f,  0.5f );
+			if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX*3, 0); } glVertex3f(  0.5f, -0.5f, -0.5f );
+			if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX*3, 1); } glVertex3f(  0.5f,  0.5f, -0.5f );
+			if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX*2, 1); } glVertex3f(  0.5f,  0.5f,  0.5f );
+		}
+		glEnd();
+
+		// Render the back quad
+		glBegin(GL_QUADS);
+		{
+			if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX, 0); } glVertex3f( -0.5f, -0.5f,  0.5f );
+			if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX*2, 0); } glVertex3f(  0.5f, -0.5f,  0.5f );
+			if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX*2, 1); } glVertex3f(  0.5f,  0.5f,  0.5f );
+			if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX, 1); } glVertex3f( -0.5f,  0.5f,  0.5f );
+		}
+		glEnd();
+
+		// Render the right quad
+		glBegin(GL_QUADS);
+		{
+			if(mode == TEXTURED_RENDER){ glTexCoord2f(0,		0); }		glVertex3f( -0.5f, -0.5f, -0.5f );
+			if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX, 0); } glVertex3f( -0.5f, -0.5f,  0.5f );
+			if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX, 1); } glVertex3f( -0.5f,  0.5f,  0.5f );
+			if(mode == TEXTURED_RENDER){ glTexCoord2f(0,		1); } glVertex3f( -0.5f,  0.5f, -0.5f );
+		}
+		glEnd();
+
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+		// Restore enable bits and matrix
+		glPopAttrib();
 	}
-	glDisable(GL_DEPTH_TEST);
-	glDisable(GL_LIGHTING);
-	glDisable(GL_BLEND);
-
-	// Just in case we set all vertices to white.
-	if(mode == TEXTURED_RENDER)
-	{
-		glColor4f(1,1,1,1);
-	} else {
-		glColor(Vector3::fromRGB(119, 196, 242));
-	}
-
-	const float deltaX = 0.25f;
-
-	// Render the front quad
-	glBindTexture(texture);
-	glBegin(GL_QUADS);
-	{
-		if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX*3, 0); } glVertex3f(  0.5f, -0.5f, -0.5f );
-		if(mode == TEXTURED_RENDER){ glTexCoord2f(1,		  0); } glVertex3f( -0.5f, -0.5f, -0.5f );
-		if(mode == TEXTURED_RENDER){ glTexCoord2f(1,		  1); } glVertex3f( -0.5f,  0.5f, -0.5f );
-		if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX*3, 1); } glVertex3f(  0.5f,  0.5f, -0.5f );
-	}
-	glEnd();
-
-	// Render the left quad
-	glBegin(GL_QUADS);
-	{
-		if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX*2, 0); } glVertex3f(  0.5f, -0.5f,  0.5f );
-		if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX*3, 0); } glVertex3f(  0.5f, -0.5f, -0.5f );
-		if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX*3, 1); } glVertex3f(  0.5f,  0.5f, -0.5f );
-		if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX*2, 1); } glVertex3f(  0.5f,  0.5f,  0.5f );
-	}
-	glEnd();
-
-	// Render the back quad
-	glBegin(GL_QUADS);
-	{
-		if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX, 0); } glVertex3f( -0.5f, -0.5f,  0.5f );
-		if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX*2, 0); } glVertex3f(  0.5f, -0.5f,  0.5f );
-		if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX*2, 1); } glVertex3f(  0.5f,  0.5f,  0.5f );
-		if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX, 1); } glVertex3f( -0.5f,  0.5f,  0.5f );
-	}
-	glEnd();
-
-	// Render the right quad
-	glBegin(GL_QUADS);
-	{
-		if(mode == TEXTURED_RENDER){ glTexCoord2f(0,		0); }		glVertex3f( -0.5f, -0.5f, -0.5f );
-		if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX, 0); } glVertex3f( -0.5f, -0.5f,  0.5f );
-		if(mode == TEXTURED_RENDER){ glTexCoord2f(deltaX, 1); } glVertex3f( -0.5f,  0.5f,  0.5f );
-		if(mode == TEXTURED_RENDER){ glTexCoord2f(0,		1); } glVertex3f( -0.5f,  0.5f, -0.5f );
-	}
-	glEnd();
-
-	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
-	// Restore enable bits and matrix
-	glPopAttrib();
 	glPopMatrix();
 }
