07ca9d3a1608bf7078403ac133877815fd8c113f
Ignacio Avas <gmail@ignacioavas.com>
2014-05-17 16:42:10 -0300

    Flappy rendered as a solidCube with key handling and movement

--- FlappyBird/FlappyBird/Flappy.cpp
+++ FlappyBird/FlappyBird/Flappy.cpp
@@ -1,56 +1,63 @@
 #include "Flappy.h"
 #include "Vector3.h" 
+#include <GL/freeglut.h>
 
-Flappy::Flappy():
-	texture("icon.png", true, true),
-	animTime(0)
+const float Flappy::maxHeight = 210;
+const float Flappy::gravity = -1000;
+const float Flappy::gravity2 = -2;
+const Vector3 Flappy::displacement(-40, 0, 0);
+const float Flappy::velocityJump = 60;
+
+#define max(x,y) ((x)>(y)?x:y)
+
+Flappy::Flappy(Bridge *colliderBridge):
+	height(120),
+	colliderBridge(colliderBridge),
+	velocity(0),
+	acceleration(0),
+	acceleration2(0)
 {
 }
 
+void Flappy::handleEvent(Scene &, const SDL_Event& ev)
+{
+	// space or left mouse button
+	if(ev.type == SDL_KEYDOWN && !ev.key.repeat && ev.key.keysym.scancode == SDL_SCANCODE_SPACE ||
+		ev.type == SDL_MOUSEBUTTONDOWN && ev.button.button == SDL_BUTTON_LEFT)
+	{
+		acceleration2 = acceleration = 0;
+		velocity = velocityJump;
+	}
+}
+
+
 void Flappy::render(Scene &parent)
 {
-	// pop texture animation
-	glMatrixMode(GL_TEXTURE);
-	glPushMatrix();
-	glLoadIdentity();
-	glTranslatef(animTime, 0, 0);
-	glMatrixMode(GL_MODELVIEW);	
+	// recalculate height, accel velocity
+	float frameTime = parent.app().getFrameTime();
+	acceleration2 += gravity2 * frameTime;
+	acceleration += acceleration2 * (-gravity) * frameTime;
+	velocity += acceleration * frameTime;
+	height += velocity  * frameTime;
 
-	glPushAttrib(GL_ENABLE_BIT);
-	glDisable(GL_DEPTH_TEST);
-	glToggle(GL_TEXTURE_2D, parent.app().getOptions()->getTexturesEnabled());
 
-	glMatrixMode(GL_PROJECTION);
-	glPushMatrix();
+	float minHeight = colliderBridge->getStreetHeight();
+	if(height > maxHeight)
 	{
-		glLoadIdentity();
-		glOrtho(0, 1, 0, 1, -1, 1);
-		glMatrixMode(GL_MODELVIEW);
-		glPushMatrix();
-		{
-			glLoadIdentity();
-			glBindTexture(texture);
-			glBegin(GL_QUADS);
-			{	//TEXTURE				//QUAD
-				glTexCoord2f(0, 0);		glVertex2f  (0.3f, 0.6f);
-				glTexCoord2f(0, 1);		glVertex2f  (0.3f, 0.5f);
-				glTexCoord2f(1, 1);		glVertex2f  (0.4f, 0.5f);
-				glTexCoord2f(1, 0);		glVertex2f  (0.4f, 0.6f);
-			}
-			glEnd();
-		}
-		glPopMatrix();
+		velocity = acceleration = acceleration2 = 0;
+		height = maxHeight;
+	} if(height < minHeight)
+	{
+		velocity = acceleration = acceleration2 = 0;
+		height = minHeight;
 	}
 
-	// restore matrices
-	glMatrixMode(GL_PROJECTION);
+	glColor(Vector3::fromRGB(255, 0, 0));
+	glPushMatrix();
+	{
+		glTranslate(height * Vector3::up +  displacement);
+		glRotate(atanf(velocity/ (2 * velocityJump)) * 180 / (GLfloat)M_PI, Vector3::forward);
+		glutSolidCube(7);
+	}
 	glPopMatrix();
-	glMatrixMode(GL_TEXTURE);
-	glPopMatrix();	
-	glMatrixMode(GL_MODELVIEW);
-
-	glPopAttrib();
-
-	float multiplier = parent.app().getOptions()->speedMultiplier();
-
 }
--- FlappyBird/FlappyBird/Flappy.h
+++ FlappyBird/FlappyBird/Flappy.h
@@ -1,14 +1,26 @@
 #pragma once
 #include "SceneObject.h"
 #include "Texture.h"
+#include "Vector3.h"
+#include "Bridge.h"
 
 class Flappy: public SceneObject
 {
 private:
-	Texture texture;
-	GLfloat animTime;
+	static const float maxHeight;
+	static const Vector3 displacement; // bird displacement from the center
+	static const float gravity; 
+	static const float gravity2; 
+	static const float velocityJump;
+
+	Bridge *colliderBridge;
+	GLfloat height;
+	GLfloat velocity;
+	GLfloat acceleration;
+	GLfloat acceleration2;
 public:
-	Flappy();
+	Flappy(Bridge *colliderBridge);
 
 	void render(Scene &parent);
+	void handleEvent(Scene &parent, const SDL_Event& ev);
 };
