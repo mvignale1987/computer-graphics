a0f99a657bb660d9ac65355981de7f1fd122e059
Mauricio Vignale <mvignale1987@gmail.com>
2014-06-23 20:06:44 -0300

    hago push de la deteccion del cilindro que se rompe por ahora

--- Raytracer/Raytracer/Scene.cpp
+++ Raytracer/Raytracer/Scene.cpp
@@ -319,8 +319,8 @@ vector<SceneObject *> Scene::readSceneObjects(Scene& scene, const xml_node &scen
 		case Shape::SPHERE:
 			res.push_back(new SceneObjectSphere(material, position, shapeDefinition->asSphere().radius()));
 			break;
-		case Shape::CYLLINDER:
-			res.push_back(new SceneObjectCyllinder(material, position, shapeDefinition->asCyllinder().radius(),shapeDefinition->asCyllinder().height()));
+//		case Shape::CYLLINDER:
+//			res.push_back(new SceneObjectCyllinder(material, position, shapeDefinition->asCyllinder().radius(),shapeDefinition->asCyllinder().height()));
 		case Shape::QUAD:
 			res.push_back(new SceneObjectQuad(
 				material,
