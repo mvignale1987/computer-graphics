7a9c09854050d41bfad7f35d56a202c39f155240
Ignacio Avas <gmail@ignacioavas.com>
2014-06-26 17:27:14 -0300

    SceneObject maneja coordenadas de texturas

--- Raytracer/Raytracer/SceneObject.h
+++ Raytracer/Raytracer/SceneObject.h
@@ -3,6 +3,7 @@
 #include "Material.h"
 #include "Intersection.h"
 #include "Ray.h"
+#include "Vector2.h"
 
 class SceneObject
 {
@@ -16,4 +17,5 @@ public:
 	Material *material() const;
 	virtual Intersection intersection(const Ray& r) = 0;
 	virtual Vector3 normalAt(const Ray& r, const Vector3& point) = 0;
+	virtual Vector2 textureCoordinatesAt(const Vector3& point) = 0;
 };
